b068b2c973999af3da5aa28e33288cc681a674a0
BigRay1337 <biggamerray@gmail.com>
2026-09-11 22:38:13 -0500

    Remove Date.now setting from content script

--- contentScript.js
+++ contentScript.js
@@ -3,24 +3,37 @@ let speedConfig = {
   cbSetIntervalChecked: true,
   cbSetTimeoutChecked: false,
   cbPerformanceNowChecked: false,
-  cbDateNowChecked: true,
   cbRequestAnimationFrameChecked: false,
 };
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.command == "setSpeedConfig") {
-    speedConfig = request.config;
-    window.postMessage(request);
-  } else if (request.command == "getSpeedConfig") {
-    sendResponse(speedConfig);
-  }
-});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.command === "setSpeedConfig") {
+    speedConfig = {
+      speed: Number(request.config?.speed) || 0,
+      cbSetIntervalChecked: !!request.config?.cbSetIntervalChecked,
+      cbSetTimeoutChecked: !!request.config?.cbSetTimeoutChecked,
+      cbPerformanceNowChecked: !!request.config?.cbPerformanceNowChecked,
+      cbRequestAnimationFrameChecked: !!request.config?.cbRequestAnimationFrameChecked,
+    };
 
-window.addEventListener("message", (e) => {
-  if (e.data.command === "getSpeedConfig") {
     window.postMessage({
       command: "setSpeedConfig",
       config: speedConfig,
     });
+
+    sendResponse({ ok: true });
+  } else if (request.command === "getSpeedConfig") {
+    sendResponse(speedConfig);
   }
+
+  return true;
+});
+
+window.addEventListener("message", (e) => {
+  if (!e.data || e.data.command !== "getSpeedConfig") return;
+
+  window.postMessage({
+    command: "setSpeedConfig",
+    config: speedConfig,
+  });
 });
