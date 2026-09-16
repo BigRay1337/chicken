2d7ad4a7faefbb852954284eab054ca92be410c6
BigRay1337 <biggamerray@gmail.com>
2026-09-16 01:15:19 -0500

    Force cbDateNowChecked false/true on extension lifecycle

--- contentScript.js
+++ contentScript.js
@@ -13,6 +13,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     window.postMessage(request);
   } else if (request.command == "getSpeedConfig") {
     sendResponse(speedConfig);
+  } else if (request.command === "extensionDisabled") {
+    speedConfig.cbDateNowChecked = false;
+    window.postMessage({ command: "extensionDisabled" });
+  } else if (request.command === "extensionEnabled") {
+    speedConfig.cbDateNowChecked = true;
+    window.postMessage({ command: "extensionEnabled" });
   }
 });
 
