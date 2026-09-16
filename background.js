20fdba4fa8390d8c08e6e91a874be76b7ad87aeb
BigRay1337 <biggamerray@gmail.com>
2026-09-16 01:15:15 -0500

    Detect extension disable/enable and update Date.now state

--- background.js
+++ background.js
@@ -8,3 +8,23 @@ chrome.runtime.onInstalled.addListener((details) => {
     });
   }
 });
+
+const sendExtensionState = async (command) => {
+  try {
+    const tabs = await chrome.tabs.query({});
+    await Promise.all(tabs.map((tab) => {
+      if (!tab.id) return Promise.resolve();
+      return chrome.tabs.sendMessage(tab.id, { command }).catch(() => {});
+    }));
+  } catch (e) {}
+};
+
+chrome.management.onDisabled.addListener((info) => {
+  if (info.id !== chrome.runtime.id) return;
+  sendExtensionState("extensionDisabled");
+});
+
+chrome.management.onEnabled.addListener((info) => {
+  if (info.id !== chrome.runtime.id) return;
+  sendExtensionState("extensionEnabled");
+});
