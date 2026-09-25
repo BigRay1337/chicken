66e152d05c5233792a37b4ea1bcbc87f93d58a8f
BigRay1337 <biggamerray@gmail.com>
2026-09-25 08:11:25 -0500

    Sync Date.now checkbox state from page refresh helper

--- contentScript.js
+++ contentScript.js
@@ -17,10 +17,14 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 window.addEventListener("message", (e) => {
+  if (!e.data) return;
+
   if (e.data.command === "getSpeedConfig") {
     window.postMessage({
       command: "setSpeedConfig",
       config: speedConfig,
     });
+  } else if (e.data.command === "setSpeedConfig" && e.data.config) {
+    speedConfig = e.data.config;
   }
 });
